da9de8541c54dabcf2bbb81ee48fc34c1ef0130e
Paul Dewaal <pgdewaal21@protonmail.com>
2019-12-12 17:53:58 -0800

    Initial Commit

---
+++ StructionSiteCC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StructionSiteCC
+//
+//  Created by Paul Dewaal on 12/12/19.
+//  Copyright © 2019 Dekabe Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
